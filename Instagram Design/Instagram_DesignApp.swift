edcb094eb2b1cd2fc91cf81c1e2db0255b665d8e
Murat Gültürk <m.gueltuerk@gmx.de>
2022-05-16 14:03:53 +0200

    Initial Commit

---
+++ Instagram Design/Instagram_DesignApp.swift
@@ -0,0 +1,17 @@
+//
+//  Instagram_DesignApp.swift
+//  Instagram Design
+//
+//  Created by Murat Gültürk on 16.05.22.
+//
+
+import SwiftUI
+
+@main
+struct Instagram_DesignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
